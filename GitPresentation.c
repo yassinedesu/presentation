709d687beea4efc894deadaac3610b191eb0e445
youcode safi <elmostainemohammed@gamil.com>
2024-09-24 11:29:25 +0100

    change

--- GitPresentation.c
+++ GitPresentation.c
@@ -3,13 +3,7 @@
 
 int main() {
     // Write C code here
-<<<<<<< HEAD
     printf("Presentation de Git Team A aaaa");
 
-=======
-    printf("Presentation de Git Team A ");
-    //Team A
-    /*Merci pour Votre attention*/
->>>>>>> refs/remotes/origin/main
     return 0;
 }
